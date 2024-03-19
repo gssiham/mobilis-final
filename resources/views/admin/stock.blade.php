b8143fd4264e0bc4b91845674cd28badd4a2846d
gssiham <guesmiaichasiham@gmail.com>
2024-03-19 01:13:36 +0100

    first commit

---
+++ resources/views/admin/stock.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <!-- <h1>
+        Articles
+        <a href="{{ route('articles.create') }}" class="btn btn-light btn-sm rounded-circle">
+            <i class="fas fa-plus"></i>
+        </a>
+    </h1> -->
+
+    @if(session('success'))
+    <div class="alert alert-success">
+        {{ session('success') }}
+    </div>
+    @endif
+    <div class="display-6 text-center mt-3 mb-3
+    
+    ">Total des articles : {{$totalarticles}}</div>
+    <table class="table w-50 table-bordered ms-auto me-auto">
+        <thead class="">
+            <tr>
+                <th>Code Bare</th>
+                <th>Designation</th>
+                <th>Quantité</th>
+                <th>Bureau</th>
+                <th>Status</th>
+                <!-- <th>Action</th> -->
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($articles as $article)
+            <tr>
+                <td>{{ $article->code_bar }}</td>
+                <td>{{ $article->designation }}</td>
+                <td>{{ $article->quantite }}</td>
+                <td>
+                    @foreach($article->offices as $office)
+                    {{ $office->num_bureau }}
+                    @if(!$loop->last), @endif
+                    @endforeach
+                </td>
+                <td>{{ $stockStatus[$article->id] }}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
+@endsection
